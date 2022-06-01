eecc7543a6280cb2a2604fbd3b19f9de3920b70f
Saad Khatri <82932419+mr-saad@users.noreply.github.com>
2022-06-01 13:16:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Pusher from "pusher-js";
 import Graphics from "./Graphics";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:5555",
+  baseURL: "https://mrakedits.herokuapp.com",
 });
 
 var pusher = new Pusher("d45ee330ac2261b51eef", {
